335dcbc6f3c8aa8466093259beae069da111fc9c
vu.nguyenhoang <rockmanx000@gmail.com>
2023-11-30 17:59:27 +0700

    add vuejs store

--- chronostore-storefront/tailwind.config.js
+++ chronostore-storefront/tailwind.config.js
@@ -1,6 +1,10 @@
+// tailwind.config.js
+import { tailwindConfig } from '@storefront-ui/vue/tailwind-config';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  presets: [tailwindConfig],
+  content: ['./index.html', './**/*.vue', './node_modules/@storefront-ui/vue/**/*.{js,mjs}'],
   theme: {
     extend: {},
   },
